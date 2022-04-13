b094254f019ce35df66c86b38b263f9ad70492a6
Ankit Tiwari <99427307+ankit1395@users.noreply.github.com>
2022-04-13 15:17:07 +0530

    Delete http-common.js

--- http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export default axios.create({
-    baseURL: 'http://localhost:8080/api/Jobs/',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  
